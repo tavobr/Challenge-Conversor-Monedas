a60c01fc3d4a3783dd56ee9f02829e8b7f1d56a0
Gustavo Borbon <gustavobr351994@gmail.com>
2025-06-02 23:19:45 -0600

    Ultima version a las 23:19

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
 import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/b9824fa47744270ff639d776/latest/USD"))
+                .build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
     }
 }
